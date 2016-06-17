0f3f9196a62cdbe0fe8e54dfdda3dc64b0e28b50
Kevin Ndung'u <kevgathuku@gmail.com>
2016-06-17 22:33:27 +0300

    Use render for tests that check for content

--- test/avatar.spec.js
+++ test/avatar.spec.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
 import {expect} from 'chai';
 
 import Avatar from '../lib/avatar';
 
 describe('<Avatar/>', function () {
   it('should have an image to display the gravatar', function () {
-    const wrapper = shallow(<Avatar/>);
+    const wrapper = render(<Avatar/>);
     expect(wrapper.find('img')).to.have.length(1);
   });
 
--- test/email.spec.js
+++ test/email.spec.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { render, shallow } from 'enzyme';
 import {expect} from 'chai';
 
 import Email from '../lib/email';
 
 describe('<Email>', function () {
   it('should have an input for the email', function () {
-    const wrapper = shallow(<Email/>);
+    const wrapper = render(<Email/>);
     expect(wrapper.find('input')).to.have.length(1);
   });
 
   it('should have a button', function () {
-    const wrapper = shallow(<Email/>);
+    const wrapper = render(<Email/>);
     expect(wrapper.find('button')).to.have.length(1);
   });
 
